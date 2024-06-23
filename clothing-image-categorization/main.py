0770b00bb9e275a1a6f40f8731f21b53b4e0b67b
r-keenan <ross.keenan@protonmail.comm>
2024-06-23 17:31:50 -0500

    updated to stop training once accuracy hits 95% to prevent overfitting.

--- clothing-image-categorization/main.py
+++ clothing-image-categorization/main.py
@@ -3,16 +3,25 @@ from tensorflow.keras.layers import Dense, Flatten
 
 import tensorflow as tf
 
-data = tf.keras.datasets.fashion_mnist
 
-(training_images, training_labels), (test_images, test_labels) = data.load_data()
+class myCallback(tf.keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs={}):
+        if logs.get("accuracy") > 0.95:
+            print("\nReached 95% accuracy so canceling training!")
+            self.model.stop_training = True
+
+
+callbacks = myCallback()
+mnist = tf.keras.datasets.fashion_mnist
+
+(training_images, training_labels), (test_images, test_labels) = mnist.load_data()
 
 training_images = training_images / 255.0
 test_images = test_images / 255.0
 
 model = tf.keras.models.Sequential(
     [
-        tf.keras.layers.Flatten(input_shape=(28, 28)),
+        tf.keras.layers.Flatten(),
         tf.keras.layers.Dense(128, activation=tf.nn.relu),
         tf.keras.layers.Dense(10, activation=tf.nn.softmax),
     ]
@@ -22,8 +31,8 @@ model.compile(
     optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"]
 )
 
-model.fit(training_images, training_labels, epochs=50)
+model.fit(training_images, training_labels, epochs=50, callbacks=[callbacks])
 
-classifications = model.predict(test_images)
-print(classifications[0])
-print(test_labels[0])
+# classifications = model.predict(test_images)
+# print(classifications[0])
+# print(test_labels[0])
